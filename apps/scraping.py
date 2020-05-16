5d6effdea647632a5903551865c06043540b853a
CocoLB <coco@cocolevant.com>
2020-05-16 15:01:33 -0700

    Challenge Completed

--- apps/scraping.py
+++ apps/scraping.py
@@ -12,14 +12,25 @@ def scrape_all():
    # set news title and paragraph variables
    news_title, news_paragraph = mars_news(browser)
    # Run all scraping functions and store results in dictionary
+   hemispheres = mars_4_hemispheres()
    data = {
        "news_title": news_title,
        "news_paragraph": news_paragraph,
        "featured_image": featured_image(browser),
        "facts": mars_facts(),
-       "last_modified": dt.datetime.now()
+       "last_modified": dt.datetime.now(),
+       "hem_0_url": hemispheres[0]['img_url'],
+       "hem_0_title":hemispheres[0]['title'],
+       "hem_1_url": hemispheres[1]['img_url'],
+       "hem_1_title":hemispheres[1]['title'],
+       "hem_2_url": hemispheres[2]['img_url'],
+       "hem_2_title":hemispheres[2]['title'],
+       "hem_3_url": hemispheres[3]['img_url'],
+       "hem_3_title":hemispheres[3]['title'],    
        }
    browser.quit()
+  
+
    return data   
 
 # Set the executable path and initialize the chrome browser in splinter
@@ -103,14 +114,62 @@ def mars_facts():
         return None
     
     #Assign columns and set index of dataframe 
-    df.columns=['Description', 'Value']
+    df.columns=['Description', 'Mars']
     df.set_index('Description', inplace=True)
     
+    # do the same for earth, get the table facts with same index, 
+    # add the earth column to the Mars facts table, and drop the 2 bottom rows
+    try:
+        df_earth = pd.read_html('https://space-facts.com/earth/')[0] 
+    except BaseException:
+        return None    
+    df_earth.columns=['description', 'Earth']
+    df_earth.set_index('description', inplace=True)
+    
+    df["Earth"] = df_earth["Earth"]
+    df=df.drop(index =['First Record:', 'Recorded By:'])
+  
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html()
 
 
-#browser.quit()
+### CHALLENGE getting the 4 Mars hemispheres images
+
+def mars_4_hemispheres():
+    hemispheres = []
+    
+    def Mars_hemisphere(i):
+        browser = Browser("chrome", executable_path="chromedriver.exe", headless=True)
+        url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+        browser.visit(url)
+    
+        links_found = browser.links.find_by_partial_text('Enhanced')
+        links_found[i].click()
+    
+        html = browser.html
+        soup = BeautifulSoup(html, 'html.parser')
+    
+        info = soup.find('div', class_='downloads').find('a')
+        link = info.get('href')
+        title = soup.find('h2', class_='title').get_text()
+
+        browser.quit()
+    
+        return ({"img_url": link, "title": title})
+
+    hem0 = Mars_hemisphere(0)
+    hem1 = Mars_hemisphere(1)
+    hem2 = Mars_hemisphere(2)   
+    hem3 = Mars_hemisphere(3)
+    
+    hemispheres.append(hem0)
+    hemispheres.append(hem1)
+    hemispheres.append(hem2)
+    hemispheres.append(hem3)
+
+    return hemispheres
+   
+
 
 if __name__ == "__main__":
     # If running as script, print scraped data
